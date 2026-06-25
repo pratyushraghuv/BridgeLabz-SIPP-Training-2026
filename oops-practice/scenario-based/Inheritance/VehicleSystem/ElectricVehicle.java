d5e63841aa49ecff081068b3a4e8d0860648371c
pratyushraghuv <raghuvanshipratyush01@gmail.com>
2026-06-25 09:57:02 +0530

    [Pratyush]Add: added inheritance problems

---
+++ oops-practice/scenario-based/Inheritance/VehicleSystem/ElectricVehicle.java
@@ -0,0 +1,20 @@
+public class ElectricVehicle extends Vehicle {
+    int batteryCapacity; // in kWh
+
+    ElectricVehicle(String model, int maxSpeed, int batteryCapacity) {
+        super(model, maxSpeed);
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    void charge() {
+        System.out.println("Charging " + model + " with " + batteryCapacity + " kWh battery.");
+    }
+
+    @Override
+    void displayInfo() {
+        System.out.println("Electric Vehicle:");
+        super.displayInfo();
+        System.out.println("Battery Capacity: " + batteryCapacity + " kWh");
+        System.out.println("-------------------------");
+    }
+}
